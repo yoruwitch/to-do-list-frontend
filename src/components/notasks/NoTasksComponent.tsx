8c808a96204da09c7d8ccbccf1f37662ac317fba
yoruwitch <evelyn.fernandes4@gmail.com>
2025-02-13 22:23:20 -0300

    feat: add 404 image

--- src/components/notasks/NoTasksComponent.tsx
+++ src/components/notasks/NoTasksComponent.tsx
@@ -1,8 +1,11 @@
+import scarecrow from "../../assets/scarecrow404.png";
+import "./notasks.css";
 function NoTasksComponent() {
     return (
         <>
             <div className="empty_taskboard">
-                <span>You don't have a new task yet</span>
+                <img src={scarecrow} alt="Logo" id="image_404" />
+                <span>You don't have a new task yet.</span>
             </div>
         </>
     );
